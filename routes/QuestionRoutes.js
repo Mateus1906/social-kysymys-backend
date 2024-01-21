17e07b9adf858d4fb5149322118b9111cb822611
Mateus1906 <114652380+Mateus1906@users.noreply.github.com>
2024-01-21 11:16:31 +0000

    dimanche morning new file:   .env new file:   db.js modified:   package-lock.json modified:   package.json modified:   routes/AnswerRoutes.js new file:   routes/Index.js modified:   routes/QuestionRoutes.js

--- routes/QuestionRoutes.js
+++ routes/QuestionRoutes.js
@@ -34,3 +34,39 @@ router.post("/", async (req,res) => {
             });      
         }
     });
+
+    //Get all questions with its answers
+    //GET /api/questions - GET ALL QUESTIONS
+router.get("/", async (req, res) => {
+    try {
+        //Perform a MongoDB 'join' query to fetch all answers for questions based on the questionID field
+      await questionDB
+        .aggregate([
+          {
+            $lookup: {
+              from: "answers",
+              localField: "_id",
+              foreignField: "questionId",
+              as: "allAnswers",
+            },
+          },
+        ])
+        .exec()
+        .then((doc) => {
+          res.status(200).send(doc);
+        })
+        .catch((error) => {
+          res.status(500).send({
+            status: false,
+            message: "Unable to get the questions",
+          });
+        });
+    } catch (err) {
+      res.status(500).send({
+        status: false,
+        message: "Server error while getting questions",
+      });
+    }
+  });
+  
+  module.exports = router;
